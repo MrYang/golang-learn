a11cd8083d9573661fe2da28a5abd40547c53d6a
Mr.Yang <paul.wide@gmail.com>
2018-06-04 17:13:01 +0800

    添加websocket

--- server/http/http.go
+++ server/http/http.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 
 	"zz.com/go-study/conf"
+	"golang.org/x/net/websocket"
+	gws "github.com/gorilla/websocket"
+	"fmt"
+	"os"
+	"io/ioutil"
 )
 
 func Start() {
@@ -15,6 +20,90 @@ func Start() {
 		query := r.Form.Get("query")
 		log.Println("query:", query)
 		RenderMsgJson(w, "test")
+	})
+
+	http.Handle("/ws", websocket.Handler(func(ws *websocket.Conn) {
+		writeMsg := make(chan string, 1)
+		exitChan := make(chan struct{})
+		go func() {
+			for {
+				var receive string
+				if err := websocket.Message.Receive(ws, &receive); err != nil {
+					exitChan <- struct{}{}
+					break
+				}
+				writeMsg <- receive
+			}
+		}()
+		for {
+			select {
+			case receive := <-writeMsg:
+				msg := "reply " + receive
+				if err := websocket.Message.Send(ws, msg); err != nil {
+					break
+				}
+			case <-exitChan:
+				break
+			}
+
+		}
+	}))
+
+	http.HandleFunc("/gws", func(w http.ResponseWriter, r *http.Request) {
+		upgrader := gws.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+
+		writeMsg := make(chan string, 1)
+		exitChan := make(chan struct{})
+
+		go func() {
+			for {
+				msgType, msg, err := conn.ReadMessage()
+				log.Println("msgType", msgType)
+				if err != nil {
+					exitChan <- struct{}{}
+					break
+				}
+
+				writeMsg <- string(msg)
+			}
+
+		}()
+
+		for {
+			select {
+			case msg := <-writeMsg:
+				if conn.WriteMessage(gws.TextMessage, []byte("reply "+string(msg))) != nil {
+					break
+				}
+			case <-exitChan:
+				break
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		indexFile, err := os.Open("http/ws.html")
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		index, err := ioutil.ReadAll(indexFile)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		fmt.Fprintf(w, string(index))
 	})
 
 	port := conf.Config().Server.Http.Port
